4a057093e5874d4eb8abb49bfe2314b50b6f2433
Harshit <devinnow8@gmail.com>
2023-01-09 11:46:56 +0530

    update validations on home screen

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ import {
   Input,
   Button,
 } from "reactstrap";
-import { useState } from "react";
-import { idType, nationalityOptions } from "../constants";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,10 +30,29 @@ export default function Home() {
     { value: "Premium Lounge(DLA)", label: "Premium Lounge(DLA)" },
     { value: "NIN Enrollment", label: "NIN Enrollment" },
   ];
-
+  const nationalityOptions = [
+    { value: "Nigeria", label: "Nigeria" },
+    { value: "India", label: "India" },
+    { value: "Japan", label: "Japan" },
+    { value: "Singapore", label: "Singapore" },
+    { value: "Switzerland", label: "Switzerland" },
+  ];
+  const idType = [
+    { value: "Driving Licence", label: "Driving Licence" },
+    { value: "Passport", label: "Passport" },
+  ];
+  const { push } = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const [selectedService, setSelectedService] = useState(serviceOptions[1]);
-
+  const [isValidation, setIsValidation] = useState(false);
+  const [validationsError, setValidationsError] = useState({
+    application_id: "",
+    dob: "",
+    name: "",
+    nationality: "",
+    id_type: "",
+    id_number: "",
+  });
   const [inputFields, setInputFields] = useState({
     application_id: "",
     dob: "",
@@ -42,7 +61,7 @@ export default function Home() {
     id_type: "",
     id_number: "",
   });
-  console.log("inputFields: ", inputFields);
+
   const handle_change = (e) => {
     const key = e.target.id;
     const temp = { ...inputFields };
@@ -62,6 +81,101 @@ export default function Home() {
   };
 
   const navbarToggle = () => setIsOpen(!isOpen);
+
+  const handleContinue = () => {
+    setIsValidation(true);
+    if (selectedService.label !== "Visa") {
+      if (
+        inputFields.name.trim() !== "" &&
+        inputFields.nationality.label.trim() !== "" &&
+        inputFields.id_type.label.trim() !== "" &&
+        inputFields.id_number.trim() !== ""
+      ) {
+        push("/book-appointment");
+      } else {
+        let errors = { ...validationsError };
+
+        if (!inputFields.name.trim()) errors.name = "This Field is required";
+        else errors.name = "";
+
+        if (!inputFields.nationality.label?.trim())
+          errors.nationality = "This Field is required";
+        else errors.nationality = "";
+
+        if (!inputFields.id_type.label?.trim())
+          errors.id_type = "This Field is required";
+        else errors.id_type = "";
+
+        if (!inputFields.id_number.trim())
+          errors.id_number = "This Field is required";
+        else errors.id_number = "";
+
+        setValidationsError(errors);
+      }
+    } else {
+      if (
+        inputFields.application_id.trim() !== "" &&
+        inputFields.dob.trim() !== ""
+      ) {
+        push("/book-appointment");
+      } else {
+        let errors = { ...validationsError };
+
+        if (!inputFields.application_id.trim())
+          errors.application_id = "This Field is required";
+        else errors.application_id = "";
+
+        if (!inputFields.dob.trim()) errors.dob = "This Field is required";
+        else errors.dob = "";
+
+        setValidationsError(errors);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isValidation) {
+      if (selectedService.label !== "Visa") {
+        let errors = { ...validationsError };
+
+        if (!inputFields.name.trim()) errors.name = "This Field is required";
+        else errors.name = "";
+
+        if (!inputFields.nationality.label?.trim())
+          errors.nationality = "This Field is required";
+        else errors.nationality = "";
+
+        if (!inputFields.id_type.label?.trim())
+          errors.id_type = "This Field is required";
+        else errors.id_type = "";
+
+        if (!inputFields.id_number.trim())
+          errors.id_number = "This Field is required";
+        else errors.id_number = "";
+
+        setValidationsError(errors);
+      } else {
+        let errors = { ...validationsError };
+
+        if (!inputFields.application_id.trim())
+          errors.application_id = "This Field is required";
+        else errors.application_id = "";
+
+        if (!inputFields.dob.trim()) errors.dob = "This Field is required";
+        else errors.dob = "";
+
+        setValidationsError(errors);
+      }
+    }
+  }, [
+    isValidation,
+    inputFields.application_id,
+    inputFields.dob,
+    inputFields.id_number,
+    inputFields.id_type,
+    inputFields.name,
+    inputFields.nationality,
+  ]);
 
   return (
     <>
@@ -95,13 +209,15 @@ export default function Home() {
           <Row className="appointment-form__row">
             <Col md={6} lg={6} xl={6}>
               <div className="appointment-form__img--wrapper">
-                <Image
-                  alt="img"
-                  src="/images/appoint-img.png"
-                  className="appointment-form__img"
-                  height={380}
-                  width={490}
-                />
+                <div className="appointment-form__img">
+                  <Image
+                    alt="img"
+                    src="/images/appoint-img.png"
+                    className="appointment-form__img"
+                    height={380}
+                    width={490}
+                  />
+                </div>
               </div>
             </Col>
             <Col md={6} lg={6} xl={6}>
@@ -118,7 +234,18 @@ export default function Home() {
                     <Label for="exampleSelect">Select Service</Label>
                     <Select
                       defaultValue={selectedService}
-                      onChange={setSelectedService}
+                      onChange={(selected) => {
+                        setSelectedService(selected);
+                        setIsValidation(false);
+                        setValidationsError({
+                          application_id: "",
+                          dob: "",
+                          name: "",
+                          nationality: "",
+                          id_type: "",
+                          id_number: "",
+                        });
+                      }}
                       options={serviceOptions}
                       className="react-select-container"
                       classNamePrefix="react-select"
@@ -129,7 +256,10 @@ export default function Home() {
                       <>
                         <Col lg={6} xl={6}>
                           <FormGroup>
-                            <Label for="application_id">Application ID</Label>
+                            <Label for="application_id">
+                              Application ID
+                              <span className="star">*</span>
+                            </Label>
                             <Input
                               id="application_id"
                               name="application_id"
@@ -139,11 +269,17 @@ export default function Home() {
                               value={inputFields.application_id}
                               className="appointment-form__input"
                             />
+                            <div className="error-msg">
+                              {validationsError.application_id}
+                            </div>
                           </FormGroup>
                         </Col>
                         <Col lg={6} xl={6}>
                           <FormGroup>
-                            <Label for="exampleDate">Date of Birth</Label>
+                            <Label for="exampleDate">
+                              Date of Birth
+                              <span className="star">*</span>
+                            </Label>
                             <Input
                               id="dob"
                               name="dob"
@@ -153,6 +289,9 @@ export default function Home() {
                               onChange={handle_date_change}
                               value={inputFields.dob}
                             />
+                            <div className="error-msg">
+                              {validationsError.dob}
+                            </div>
                           </FormGroup>
                         </Col>
                       </>
@@ -167,11 +306,14 @@ export default function Home() {
                               id="name"
                               name="name"
                               type="text"
-                              placeholder="eg: John Doe"
+                              placeholder="eg: Jhone Doe"
                               className="appointment-form__input"
                               value={inputFields.name}
                               onChange={handle_change}
                             />
+                            <div className="error-msg">
+                              {validationsError.name}
+                            </div>
                           </FormGroup>
                         </Col>
                         <Col lg={6} xl={6}>
@@ -186,12 +328,16 @@ export default function Home() {
                               classNamePrefix="react-select"
                               value={inputFields.nationality}
                             />
+                            <div className="error-msg">
+                              {validationsError.nationality}
+                            </div>
                           </FormGroup>
                         </Col>
                         <Col lg={6} xl={6}>
                           <FormGroup>
                             <Label for="id_type">
-                              ID Type <span className="star">*</span>
+                              ID Type
+                              <span className="star">*</span>
                             </Label>
                             <Select
                               onChange={(e) => handle_select(e, "id_type")}
@@ -200,12 +346,16 @@ export default function Home() {
                               classNamePrefix="react-select"
                               value={inputFields.id_type}
                             />
+                            <div className="error-msg">
+                              {validationsError.id_type}
+                            </div>
                           </FormGroup>
                         </Col>
                         <Col lg={6} xl={6}>
                           <FormGroup>
                             <Label for="id_number">
-                              ID Number <span className="star">*</span>
+                              ID Number
+                              <span className="star">*</span>
                             </Label>
                             <Input
                               id="id_number"
@@ -215,15 +365,22 @@ export default function Home() {
                               onChange={handle_change}
                               value={inputFields.id_number}
                             />
+                            <div className="error-msg">
+                              {validationsError.id_number}
+                            </div>
                           </FormGroup>
                         </Col>
                       </>
                     )}
                   </Row>
                   <FormGroup className="text-md-start text-center ">
-                    <Link className="cont-btn" href="/book-appointment">
+                    <div
+                      className="cont-btn"
+                      // href="/book-appointment"
+                      onClick={handleContinue}
+                    >
                       Continue
-                    </Link>
+                    </div>
                   </FormGroup>
                 </form>
                 <p className="appointment-form_desc">
