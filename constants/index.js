1b469e35b5babbed4d3a4ccae7c7ddfd5f9c5ff0
Harshit <devinnow8@gmail.com>
2023-01-18 14:43:42 +0530

    added centers names

--- constants/index.js
+++ constants/index.js
@@ -54,9 +54,21 @@ export const monthNames = [
 ];
 
 export const centers = [
-  { value: "London UK", label: "London UK" },
-  { value: "Nigeria", label: "Nigeria" },
+  { value: "Ankara, Turkey", label: "Ankara, Turkey" },
+  { value: "Atlanta, USA", label: "Atlanta, USA" },
+  { value: "Beijing, China", label: "Beijing, China" },
+  { value: "Beirut, Lebanon", label: "Beirut, Lebanon" },
+  { value: "Dubai, UAE", label: "Dubai, UAE" },
+  { value: "Guangzhou, China", label: "Guangzhou, China" },
+  { value: "Johannesburg, SA", label: "Johannesburg, SA" },
+  { value: "Kuala Lumpur, Malaysia", label: "Kuala Lumpur, Malaysia" },
+  { value: "London, UK", label: "London, UK" },
+  { value: "Los Angeles, USA", label: "Los Angeles, USA" },
   { value: "New Delhi, India", label: "New Delhi, India" },
-  { value: "Japan", label: "Japan" },
-  { value: "Singapore", label: "Singapore" },
+  { value: "New York, US", label: "New York, US" },
+  { value: "Paris, France", label: "Paris, France" },
+  { value: "Rome, Italy", label: "Rome, Italy" },
+  { value: "Shanghai, China", label: "Shanghai, China" },
+  { value: "The Hague, Netherlands", label: "The Hague, Netherlands" },
+  { value: "Washington DC, USA", label: "Washington DC, USA" },
 ];
