28cee2e04996e5d9e6f7835f4a6e84d3bef5737d
harshit <devinnow8@gmail.com>
2023-02-03 23:15:48 +0530

    code-clean

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,8 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../components/Header";
-import { getUserAppointment } from "../redux/action/index";
-import {
-  applicationDetailsFetchRequest,
-  applicationDetailsFetchSuccess,
-  applicationDetailsFetchMemberSuccess,
-} from "../redux/reducer/application-detail";
+import { applicationDetailsFetchRequest } from "../redux/reducer/application-detail";
 import { categoryServiceListFetchRequest } from "../redux/reducer/category-service";
-import { toast } from "react-toastify";
 import ApplicationForm from "../components/home/application-form";
 
 export default function Home() {
@@ -18,70 +12,47 @@ export default function Home() {
   const { categoryServiceList } = useSelector(
     (state) => state.categoryServiceList,
   );
+  const { isLoading } = useSelector((state) => state.applicationDetails);
   const router = useRouter();
   const [categoryServiceOptions, setCategoryServiceOptions] = useState([]);
   const [selectedService, setSelectedService] = useState([]);
-  const [isLoader, setIsLoader] = useState(false);
 
   const handleContinue = (values) => {
-    setIsLoader(true);
-    const obj = {
-      selectedService: selectedService,
-      appointmentDetails: values,
-    };
-    dispatch(getUserAppointment(obj));
     const details = {
       applicationId: values.application_id,
       dob: values.dob,
       serviceType: selectedService.label,
     };
     dispatch(
-      applicationDetailsFetchRequest(
-        details,
-        (success) => {
-          if (success.data.category !== selectedService.label) {
-            setIsLoader(false);
-            toast.error("Application not found");
-          } else {
-            const tempArray = [];
-            tempArray.push(success.data);
-            dispatch(applicationDetailsFetchMemberSuccess(tempArray));
-            dispatch(applicationDetailsFetchSuccess(success.data));
-            if (success.status === 200) {
-              router.push({
-                pathname: "/book-appointment",
-                query: { selectedService: selectedService.label },
-              });
-              setIsLoader(false);
-            }
-          }
-        },
-        (error) => {
-          setIsLoader(false);
-          if (error.message.includes("Network Error")) {
-            toast.error(error.message);
-          } else {
-            toast.error("Application not found");
-          }
-        },
-      ),
+      applicationDetailsFetchRequest(details, (success) => {
+        if (success) {
+          router.push({
+            pathname: "/book-appointment",
+            query: { selectedService: selectedService.label },
+          });
+        }
+      }),
     );
+
   };
 
   useEffect(() => {
     dispatch(categoryServiceListFetchRequest());
-    const obtainedList = categoryServiceList.map((service) => {
-      return {
-        value:
-          service?.categoryName.charAt(0).toUpperCase() +
-          service?.categoryName.slice(1),
-        label:
-          service?.categoryName.charAt(0).toUpperCase() +
-          service?.categoryName.slice(1),
-      };
-    });
+    if(categoryServiceList.length > 0) {
+      const obtainedList =
+      categoryServiceList.map((service) => {
+        return {
+          value:
+            service?.categoryName.charAt(0).toUpperCase() +
+            service?.categoryName.slice(1),
+          label:
+            service?.categoryName.charAt(0).toUpperCase() +
+            service?.categoryName.slice(1),
+        };
+      });
     setCategoryServiceOptions(obtainedList);
-    setSelectedService(obtainedList[0])
+    setSelectedService(obtainedList[0]);
+    }
   }, []);
 
   return (
@@ -99,7 +70,7 @@ export default function Home() {
           setSelectedService={setSelectedService}
           categoryServiceOptions={categoryServiceOptions}
           handleContinue={handleContinue}
-          isLoader={isLoader}
+          isLoader={isLoading}
         />
       </section>
     </>
--- redux/saga/application-detail/index.js
+++ redux/saga/application-detail/index.js
@@ -1,6 +1,12 @@
 import { call, put, takeLatest } from "redux-saga/effects";
-import { applicationDetailsFetchRequest } from "../../reducer/application-detail";
+import {
+  applicationDetailsFetchRequest,
+  applicationDetailsFetchSuccess,
+  applicationDetailsFetchMemberSuccess,
+  applicationDetailsFetchFailure,
+} from "../../reducer/application-detail";
 import * as services from "../../../services";
+import { toast } from "react-toastify";
 
 function* getApplicationDetailsRequest(action) {
   try {
@@ -8,9 +14,28 @@ function* getApplicationDetailsRequest(action) {
       services.applicationDetails,
       action.payload.requestBody,
     );
-    action.payload.successCalback(response);
+    const { status, statusText, data } = response || {};
+    if (status === 200) {
+      if (data.category !== action.payload.requestBody.serviceType) {
+        yield put(applicationDetailsFetchFailure());
+        toast.error("Application not found");
+      } else {
+        yield put(applicationDetailsFetchSuccess(data));
+        const tempArray = [];
+        tempArray.push(data);
+        yield put(applicationDetailsFetchMemberSuccess(tempArray));
+        action.payload.successCallback(true);
+      }
+    } else {
+      yield put(applicationDetailsFetchFailure());
+    }
   } catch (e) {
-    action.payload.errorCallback(e);
+    yield put(applicationDetailsFetchFailure());
+    if (e.message.includes("Network Error")) {
+      toast.error(e.message);
+    } else {
+      toast.error("Application not found");
+    }
   }
 }
 
