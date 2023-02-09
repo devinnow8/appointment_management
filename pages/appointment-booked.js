233ec4665464fff2f5e2eb5c6eaa318ea81fd144
Harshit <devinnow8@gmail.com>
2023-02-09 14:33:24 +0530

    pdf implemented changes

--- pages/appointment-booked.js
+++ pages/appointment-booked.js
@@ -31,7 +31,20 @@ function AppointmentBooked() {
 
   const handlePrintSlip = (id) => {
     // window.open("https://apt.oisservices.com/app/print/NDA5NDUz", "_blank");
-    dispatch(appointmentBookedPdfRequest(id));
+    dispatch(
+      appointmentBookedPdfRequest(id, (success) => {
+        console.log(success, "successsuccess==>");
+        const file = new Blob([success.data], { type: "application/pdf" });
+        //Build a URL from the file
+        const fileURL = URL.createObjectURL(file);
+        const link = document.createElement("a");
+        link.href = fileURL;
+        link.download = "AppointmentBooked.pdf";
+        link.click();
+        //Open the URL on new Window
+        window.open(fileURL);
+      }),
+    );
   };
   return (
     <>
@@ -48,7 +61,7 @@ function AppointmentBooked() {
               <h2 className="apt-booked__title">Appointment Booked</h2>
               <p className="apt-booked__para">
                 {`Your appointment booking is complete, Appointment ID is:
-                ${appointment.application_id}. A copy of the appointment slip and checklist have been
+                ${appointment.appointment_id}. A copy of the appointment slip and checklist have been
                 sent to your email. Optionally you can download it by click the
                 buttons given below.`}
               </p>
@@ -62,7 +75,7 @@ function AppointmentBooked() {
               <div className="d-flex justify-content-center align-items-center">
                 <Button
                   className="slip-btn mb-sm-0"
-                  onClick={() => handlePrintSlip(appointment.application_id)}
+                  onClick={() => handlePrintSlip(appointment.appointment_id)}
                 >
                   Print Booking Slip
                 </Button>
