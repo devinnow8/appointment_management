8cae0034dd27fc2011e1edcab87af34f614ce6ec
harshit <devinnow8@gmail.com>
2023-02-09 20:25:42 +0530

    email validations

--- components/home/service-type/Others.js
+++ components/home/service-type/Others.js
@@ -23,11 +23,17 @@ const Others = ({ handleContinue, isLoader }) => {
       if (values.name == "") {
         errors.name = "Required";
       }
-      if (values.email == "") {
+      if (!values.email) {
         errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
       }
-      if (values.phone == "") {
+      if (!values.phone) {
         errors.phone = "Required";
+      } else if (/^[6-9]\d{9}$/.test(values.phone)) {
+        errors.phone = "Invalid Phone Number";
       }
       if (values.id_number == "") {
         errors.id_number = "Required";
