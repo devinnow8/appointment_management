5e87737870604e67a66781fcddbd0caeca933114
Harshit <devinnow8@gmail.com>
2023-03-01 09:29:37 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAl742lnszifLn2dC4DVYRKxHXZi-knQ2o",
-  authDomain: "ois-appointment-user.firebaseapp.com",
-  projectId: "ois-appointment-user",
-  storageBucket: "ois-appointment-user.appspot.com",
-  messagingSenderId: "807665768933",
-  appId: "1:807665768933:web:1e93c3e7e4bfb294def70c",
-  measurementId: "G-XCP6ZVYY0B",
+  apiKey: "AIzaSyDR_8AdmKKaDx-0v_uxNCF_QSmksHkZpWE",
+  authDomain: "ois-appointment-user-staging.firebaseapp.com",
+  projectId: "ois-appointment-user-staging",
+  storageBucket: "ois-appointment-user-staging.appspot.com",
+  messagingSenderId: "112966134216",
+  appId: "1:112966134216:web:7453725314f879fbc9f8fd",
+  measurementId: "G-V8CPRCGYZL",
 };
 
 // Initialize Firebase
